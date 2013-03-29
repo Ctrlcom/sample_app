483908928c440e6f1b89b6e1f32f980e1db53ce3
Ctrlcom <contact@ctrl-communication.com>
2013-03-29 15:40:20 +0100

    Depot Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'aefb488e794a50ad35c62f4fe72101efce339c6e945f554594655956b66ec83f86ed54d8c9a7e7db31b7405effcc1bc0702a6ca014073aad1afebc4940028605'
